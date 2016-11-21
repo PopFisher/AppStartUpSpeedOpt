d9515e4d2ab9773cb7c2db1bfc0d39e838fd9f58
popfisher <774232122@qq.com>
2016-11-21 18:22:08 +0800

    Create a project named AppStartUpSpeedOpt

---
+++ app/src/main/java/startup/app/speedopt/AppStartUpMainActivity.java
@@ -0,0 +1,13 @@
+package startup.app.speedopt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppStartUpMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(startup.app.com.appstartupspeedopt.R.layout.activity_app_start_up_main);
+    }
+}
---
+++ app/src/main/java/startup/app/speedopt/application/StartUpApplication.java
@@ -0,0 +1,10 @@
+package startup.app.speedopt.application;
+
+import android.app.Application;
+
+/**
+ * Created by popfisher on 2016/11/21.
+ */
+
+public class StartUpApplication extends Application {
+}
